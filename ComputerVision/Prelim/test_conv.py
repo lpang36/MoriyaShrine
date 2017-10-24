88090349f2ececccb0b85ed78817ee7d2db14411
Lawrence Pang <lawrencepang36@gmail.com>
2017-10-23 23:12:37 -0400

    convnet kind of works

--- ComputerVision/Prelim/test_conv.py
+++ ComputerVision/Prelim/test_conv.py
@@ -10,6 +10,7 @@ from matplotlib.patches import Rectangle
 import h5py
 
 dims = (300,400,3)
+num_images=100
 
 def load_image(filename):
 	im = Image.open(filename)
@@ -18,8 +19,8 @@ def load_image(filename):
 
 def load_all(foldername):
 	output = np.empty((0,dims[0],dims[1],dims[2]))
-	for filename in os.listdir(foldername):
-		image = np.reshape(load_image(os.path.join(foldername,filename)),(-1,dims[0],dims[1],dims[2]))
+	for i in range(0,num_images):
+		image = np.reshape(load_image(os.path.join(foldername,str(i)+'.png')),(-1,dims[0],dims[1],dims[2]))
 		output = np.concatenate((output,image),0)
 	return output.astype(np.uint8)
 
@@ -30,13 +31,13 @@ def display(image,box):
 	ax.add_patch(Rectangle((box[0],box[1]),box[2],box[3],ec='r',fc='none'))
 	plt.show()
 
-all_images = load_all('/home/lpang/Documents/GitHub/LaserTurret/ComputerVision/Images')
-
-data = pd.read_csv('/home/lpang/Documents/GitHub/LaserTurret/ComputerVision/Data/data.csv')
+data = pd.read_csv('/home/lpang/Documents/GitHub/imggen/data/data.csv')
 data['Data'] = data['Data'].apply(lambda x:np.asarray(eval(str(x))[1:]))
 y = np.empty((0,4))
 for row in data['Data']:
 	y = np.concatenate((y,np.reshape(row,(1,-1))))
+
+all_images = load_all('/home/lpang/Documents/GitHub/imggen/images')
 
 # Model Architecture
 model = Sequential()
@@ -45,6 +46,18 @@ model.add(Convolution2D(32, (5, 5), activation='relu', input_shape=dims))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Convolution2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Convolution2D(128, (2, 2), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Convolution2D(128, (2, 2), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Convolution2D(128, (2, 2), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Convolution2D(128, (2, 2), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Flatten())
--- ComputerVision/Prelim/test_conv_from_weights.py
+++ ComputerVision/Prelim/test_conv_from_weights.py
@@ -14,10 +14,10 @@ def load_image(filename):
 	pixels = np.asarray(im.getdata())
 	return pixels
 
-def load_all(foldername):
+def load_all(foldername,num_images=100):
 	output = np.empty((0,dims[0],dims[1],dims[2]))
-	for filename in os.listdir(foldername):
-		image = np.reshape(load_image(os.path.join(foldername,filename)),(-1,dims[0],dims[1],dims[2]))
+	for i in range(0,num_images):
+		image = np.reshape(load_image(os.path.join(foldername,str(i)+'.png')),(-1,dims[0],dims[1],dims[2]))
 		output = np.concatenate((output,image),0)
 	return output.astype(np.uint8)
 
@@ -27,10 +27,18 @@ def display(image,box):
 	ax = plt.gca()
 	ax.add_patch(Rectangle((box[0],box[1]),box[2],box[3],ec='r',fc='none'))
 	plt.show()
+	
+def display(image,box1,box2):
+	plt.figure()
+	plt.imshow(image)
+	ax = plt.gca()
+	ax.add_patch(Rectangle((box1[0],box1[1]),box1[2],box1[3],ec='r',fc='none'))
+	ax.add_patch(Rectangle((box2[0],box2[1]),box2[2],box2[3],ec='b',fc='none'))
+	plt.show()
 
-all_images = load_all('/home/lpang/Documents/GitHub/LaserTurret/ComputerVision/Images')
+all_images = load_all('/home/lpang/Documents/GitHub/imggen/images')
 
-data = pd.read_csv('/home/lpang/Documents/GitHub/LaserTurret/ComputerVision/Data/data.csv')
+data = pd.read_csv('/home/lpang/Documents/GitHub/imggen/data/data.csv')
 data['Data'] = data['Data'].apply(lambda x:np.asarray(eval(str(x))[1:]))
 y = np.empty((0,4))
 for row in data['Data']:
@@ -46,5 +54,5 @@ model = load_model('/home/lpang/Documents/GitHub/LaserTurret/ComputerVision/Data
 	
 results = model.predict(X_train)
 for i in range(np.shape(results)[0]):
-	print(results[i])
-	#display(X_val[i],results[i])
+	#print(results[i])
+	display(X_val[i],results[i],Y_val[i])
