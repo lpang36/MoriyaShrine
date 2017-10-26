c112658e09db7ab054608451dcd6c720efb1afad
Lawrence Pang <lawrencepang36@gmail.com>
2017-10-26 14:14:16 -0400

    get dlib model to work

---
+++ ComputerVision/Build/CMakeFiles/dnn_mmod_face_detection_ex.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lpang/Documents/GitHub/LaserTurret/ComputerVision/Model/dnn_mmod_face_detection_ex.cpp" "/home/lpang/Documents/GitHub/LaserTurret/ComputerVision/Build/CMakeFiles/dnn_mmod_face_detection_ex.dir/dnn_mmod_face_detection_ex.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/lpang/Documents/GitHub/LaserTurret/External/dlib-19.7/dlib/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/lpang/Documents/GitHub/LaserTurret/ComputerVision/Build/dlib_build/CMakeFiles/dlib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
